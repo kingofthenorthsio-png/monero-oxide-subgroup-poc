be3fa2dbce865266ddf814b1beab9c0a7f873759
kingofthenorthsio-png <kingofthenorthsio@gmail.com>
2025-09-27 04:12:56 +0000

    chore: remove target from repo and ignore it

--- target/debug/build/radium-88444380c141a68a/build_script_build-88444380c141a68a.d
+++
@@ -1,5 +0,0 @@
-C:\Users\fredb\OneDrive\Desktop\monero-oxide-subgroup-poc\monero-oxide-subgroup-poc\target\debug\build\radium-88444380c141a68a\build_script_build-88444380c141a68a.d: C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\radium-0.7.0\build.rs
-
-C:\Users\fredb\OneDrive\Desktop\monero-oxide-subgroup-poc\monero-oxide-subgroup-poc\target\debug\build\radium-88444380c141a68a\build_script_build-88444380c141a68a.exe: C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\radium-0.7.0\build.rs
-
-C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\radium-0.7.0\build.rs:
--- target/debug/deps/cfg_if-4764b5446a0ab7e6.d
+++
@@ -1,7 +0,0 @@
-C:\Users\fredb\OneDrive\Desktop\monero-oxide-subgroup-poc\monero-oxide-subgroup-poc\target\debug\deps\cfg_if-4764b5446a0ab7e6.d: C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-C:\Users\fredb\OneDrive\Desktop\monero-oxide-subgroup-poc\monero-oxide-subgroup-poc\target\debug\deps\libcfg_if-4764b5446a0ab7e6.rlib: C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-C:\Users\fredb\OneDrive\Desktop\monero-oxide-subgroup-poc\monero-oxide-subgroup-poc\target\debug\deps\libcfg_if-4764b5446a0ab7e6.rmeta: C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs
-
-C:\Users\fredb\.cargo\registry\src\index.crates.io-1949cf8c6b5b557f\cfg-if-1.0.3\src\lib.rs:
